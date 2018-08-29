cf7e83af650b2377cac22064914ad0da883d1cdc
emisto <emil1stolpe@gmail.com>
2018-08-29 21:49:26 +0200

    Testade lite med en tutorial

--- app/src/main/java/emil/packlista/MainActivity.java
+++ app/src/main/java/emil/packlista/MainActivity.java
@@ -1,7 +1,11 @@
 package emil.packlista;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addBtn = (Button) findViewById(R.id.addBtn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText textis = (EditText) findViewById(R.id.Textis);
+                EditText textis2 = (EditText) findViewById(R.id.Textis2);
+                TextView resultat = (TextView) findViewById(R.id.resultat);
+
+                int num1 = Integer.parseInt(textis.getText().toString());
+                int num2 = Integer.parseInt(textis2.getText().toString());
+                int result = num1 + num2;
+                resultat.setText(result + "");
+            }
+        });
     }
     
     public void hejEmil() {
